f6f46361f817979beca77795ed6b2467dbb93e53
AQStarLilly <kieranjhislop@gmail.com>
2025-03-22 19:56:45 -0300

    Added animator to project

--- Assets/Project Pieces/CharacterController/PlayerMovement.cs
+++ Assets/Project Pieces/CharacterController/PlayerMovement.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.Utilities;
 using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public Animator animator;
     private Rigidbody2D playerRigidbody;
     public Vector2 moveVector = Vector2.zero;
 
@@ -32,6 +33,11 @@ public class PlayerMovement : MonoBehaviour
         playerRigidbody = this.GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        HandleAnimation();
+    }
+
     private void UpdateMoveVector(Vector2 InputVector)
     {
         moveVector = InputVector;
@@ -50,6 +56,22 @@ public class PlayerMovement : MonoBehaviour
     void HandlePlayerMovement()
     {
         playerRigidbody.MovePosition(playerRigidbody.position + moveVector * currentSpeed * Time.fixedDeltaTime);
+    }
+
+    void HandleAnimation()
+    {
+        if(moveVector.magnitude != 0)
+        {
+            animator.SetFloat("Horizontal", moveVector.x);
+            animator.SetFloat("Vertical", moveVector.y);
+            animator.SetFloat("LastMoveHorizontal", moveVector.x);
+            animator.SetFloat("LastMoveVertical", moveVector.y);
+            animator.SetBool("Moving", true);
+        }
+        else
+        {          
+            animator.SetBool("Moving", false);
+        }
     }
 
     public void ResetPosition()
